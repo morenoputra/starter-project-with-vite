6f223b4cf83d93b252efdc39903d621217e8d773
moreno1308 <morenoputra201@gmail.com>
2025-11-07 07:45:44 +0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/starter-project-with-vite/sw.js', { scope: '/starter-project-with-vite/' })})}
